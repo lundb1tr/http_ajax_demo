1a17c1f3fcd34daad74f2c8dc06dbd37e3a8bb83
Tyler Lundberg <tylerl@fireflyint.com>
2020-09-08 08:17:21 -0400

    trim posts down to 4, add author to each post, added vscode settings file

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,23 +7,43 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
+  state = {
+    posts: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const posts = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      const trimmedPosts = await posts.data.slice(0, 4);
+      const addAuthors = await trimmedPosts.map(post => {
+        return {
+          ...post,
+          author: 'Tyler',
+        };
+      });
+      this.setState({ posts: addAuthors });
+    } catch (e) {
+      console.error('Fetch Error:', e);
     }
+  }
+  render() {
+    const posts = this.state.posts.map(({ id, title, author }) => {
+      return <Post key={id} title={title} author={author} />;
+    });
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
