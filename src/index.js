3dbe96bd8249628489d78a08731dc03e58b3ff79
Tyler Lundberg <tylerl@fireflyint.com>
2020-09-08 09:00:36 -0400

    using data interceptors for fetch and post requests

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+/* Potentially manipulate responses before they are processed */
+axios.interceptors.request.use(
+  config => {
+    console.log(config);
+    return config;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+/* Potentially manipulate responses before they are processed */
+axios.interceptors.response.use(
+  config => {
+    console.log(config);
+    return config;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
