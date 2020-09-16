6d306a4ead2d0fc8e127b57da76ad3d403db6c7e
Tyler Lundberg <tylerl@fireflyint.com>
2020-09-16 10:16:47 -0400

    make component aware of the route

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './Post.css';
 
-const post = ({ title, author, clicked }) => (
-  <article className="Post" onClick={clicked}>
-    <h1>{title}</h1>
-    <div className="Info">
-      <div className="Author">{author}</div>
-    </div>
-  </article>
-);
+const post = ({ clicked, title, author }) => {
+  return (
+    <article className="Post" onClick={clicked}>
+      <h1>{title}</h1>
+      <div className="Info">
+        <div className="Author">{author}</div>
+      </div>
+    </article>
+  );
+};
 
-export default post;
+/* This makes this component aware of the route */
+export default withRouter(post);
