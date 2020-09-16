b226603c4337609217534c99bf8530c332cc914f
Tyler Lundberg <tylerl@fireflyint.com>
2020-09-16 10:10:38 -0400

    removed all post references from the blog.js file

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,71 +1,37 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import axios from '../../../src/axios';
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+import { Route, Link } from 'react-router-dom';
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPost: null,
-    error: false,
-  };
-
-  async componentDidMount() {
-    try {
-      const posts = await axios.get('/posts');
-      const trimmedPosts = await posts.data.slice(0, 4);
-      const addAuthors = await trimmedPosts.map(post => {
-        return {
-          ...post,
-          author: 'Tyler',
-        };
-      });
-      this.setState({ posts: addAuthors });
-      if (this.state.error) {
-        this.setState({ error: false });
-      }
-    } catch (e) {
-      if (!this.state.error) {
-        this.setState({ error: true });
-      }
-      console.error('Fetch Error:', e);
-    }
-  }
-
-  postSelectedHandler(id) {
-    this.setState({ selectedPost: id });
-  }
-
   render() {
-    let posts = (
-      <p style={{ textAlign: 'center' }}>
-        Something went wrong getting the posts!
-      </p>
-    );
-    if (!this.state.error) {
-      posts = this.state.posts.map(({ id, title, author }) => {
-        return (
-          <Post
-            key={id}
-            title={title}
-            author={author}
-            clicked={() => this.postSelectedHandler(id)}
-          />
-        );
-      });
-    }
     return (
-      <div>
-        <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPost} error={this.state.error} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                {/* Prevents sending new request, thus reloading the app.  Only re-renders the page */}
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                {/* Prevents sending new request, thus reloading the app.  Only re-renders the page */}
+                <Link
+                  to={{
+                    pathname: '/new-post',
+                    hash: '#submit',
+                    search: '?quick-submit=true',
+                  }}
+                >
+                  New Post
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" exact component={NewPost} />
       </div>
     );
   }
