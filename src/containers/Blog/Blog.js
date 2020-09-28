3411ebe0f2981cb2002e08b13ab1d05d6549be1e
Tyler Lundberg <tylerl@fireflyint.com>
2020-09-28 07:47:00 -0400

    Switch only loads the first path that matches

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
@@ -44,10 +44,12 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Route path="/" exact component={Posts} />
-        <Route path="/new-post" exact component={NewPost} />
-        {/* :id will tell the Route to use a dynamic value to create the path */}
-        <Route path="/:id" exact component={FullPost} />
+        <Switch>
+          <Route path="/" exact component={Posts} />
+          <Route path="/new-post" exact component={NewPost} />
+          {/* :id will tell the Route to use a dynamic value to create the path */}
+          <Route path="/:id" exact component={FullPost} />
+        </Switch>
       </div>
     );
   }
