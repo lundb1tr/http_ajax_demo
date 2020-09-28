d0622064298ae96561786be120147330380bfa9f
Tyler Lundberg <tylerl@fireflyint.com>
2020-09-28 07:29:16 -0400

    back to the default active

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -13,7 +13,7 @@ class Blog extends Component {
             <ul>
               <li>
                 {/* Prevents sending new request, thus reloading the app.  Only re-renders the page */}
-                <NavLink to="/" exact activeClassName="my-active">
+                <NavLink to="/" exact activeClassName="active">
                   Home
                 </NavLink>
               </li>
